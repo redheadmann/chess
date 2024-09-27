7f586c24638acf65106e43765146d4b634c9c42d
Henry <henrykfetzer@gmail.com>
2024-09-26 23:23:40 -0600

    Just started ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,9 +10,9 @@ import java.util.Collection;
  */
 public class ChessGame {
     private ChessBoard board = new ChessBoard(); // the board on which to play
+    private TeamColor teamTurn;
 
     public ChessGame() {
-
     }
 
     /**
@@ -98,7 +98,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -107,6 +107,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return this.board;
     }
 }
--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -16,170 +16,29 @@ public class KingMovesCalculator implements PieceMovesCalculator{
 
         /* We go out in one direction at a time and stop at the edge of a board
           or when we encounter another piece
-
-         Go left, up-left, up, up-right, right, down-right, down, then down-left
         */
+        for (int rowDirection: new int[]{-1,0,1}) { // iterate at most 7 times
+            for  (int colDirection: new int[]{-1,0,1}) {
+                //make sure direction is not (0, 0)
+                if (!(rowDirection == 0 && colDirection == 0)) {
+                    int possibleRow = row + rowDirection;
+                    int possibleCol = col + colDirection;
+                    ChessPosition nextPosition = new ChessPosition(possibleRow, possibleCol);
 
-        // go left
-        int possibleCol = col - 1;
-        ChessPosition endPosition = new ChessPosition(row, possibleCol);
-
-        // if endPosition is still on the board
-        if (row > 0 && row <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(row, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
+                    // if endPosition is still on the board
+                    if (possibleRow >= 1 && possibleRow <= 8 && possibleCol >= 1 && possibleCol <= 8) {
+                        // check for a piece already at the end position
+                        ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, possibleCol));
+                        if (pieceOnPosition != null) {
+                            // check if piece on end position is of the same color
+                            if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
+                                moves.add(new ChessMove(myPosition, nextPosition, null));
+                            }
+                        } else { // we can move to any empty space
+                                moves.add(new ChessMove(myPosition, nextPosition, null));
+                        }
+                    }
                 }
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // up-left
-        int possibleRow = row + 1;
-        possibleCol = col - 1;
-        endPosition = new ChessPosition(possibleRow, possibleCol);
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // go up
-        possibleRow = row + 1;
-        endPosition = new ChessPosition(possibleRow, col);
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && col > 0 && col <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, col));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // upper right direction
-        possibleRow = row + 1;
-        possibleCol = col + 1;
-        endPosition = new ChessPosition(possibleRow, possibleCol);
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            } else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // go right
-        possibleCol = col + 1;
-        endPosition = new ChessPosition(row, possibleCol);
-
-        // if endPosition is still on the board
-        if (row > 0 && row <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(row, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // lower right direction
-        possibleRow = row - 1;
-        possibleCol = col + 1;
-        endPosition = new ChessPosition(possibleRow, possibleCol);
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            } else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // go down
-        possibleRow = row - 1;
-        endPosition = new ChessPosition(possibleRow, col);
-
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && col > 0 && col <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, col));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
-            }
-        }
-
-
-        // lower left direction
-        possibleRow = row - 1;
-        possibleCol = col - 1;
-        endPosition = new ChessPosition(possibleRow, possibleCol);
-        // if endPosition is still on the board
-        if (possibleRow > 0 && possibleRow <= 8 && possibleCol > 0 && possibleCol <= 8) {
-            // check for a piece already at the end position
-            ChessPiece pieceOnPosition = board.getPiece(new ChessPosition(possibleRow, possibleCol));
-            if (pieceOnPosition != null) {
-                // check if piece on end position is of the same color
-                //we can't capture behind an enemy piece
-                if (pieceOnPosition.getTeamColor() != teamColor) { // the piece is on the opposing team and we can capture it
-                    moves.add(new ChessMove(myPosition, endPosition, null));
-                }
-            } else {
-                moves.add(new ChessMove(myPosition, endPosition, null));
             }
         }
 
