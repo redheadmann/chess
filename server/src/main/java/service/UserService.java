0c9ca02b4b3e2c00068118865764b3f51df6b416
Henry <henrykfetzer@gmail.com>
2024-10-21 10:09:12 -0600

    Some UserService implemented

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,13 @@
 package service;
 
 public class UserService {
+
+    public record RegisterRequest(String username, String password, String email) {}
+    public record RegisterResult(String username, String authToken) {}
+
+    public RegisterResult register(RegisterRequest) {
+        // 1. get user
+        // 2. if no user create new user
+        // 3. create auth token
+    }
 }
